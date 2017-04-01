c42042a38295e5d123ac0716d141f68e9885d9b8
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-04-01 17:55:58 -0400

    add trip index view and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+trip1 = Trip.create(location: "Japan", time_span: "November 2015", photo_url: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12310536_10104182767836578_3131895657167659836_n.jpg?oh=e37d66583f21f895d38a769a3ff51c38&oe=596A6E48")
+
+
+trip2 = Trip.create(location: "Turkey", time_span: "June 2010", photo_url:"https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/1915988_928045597078_3862487_n.jpg?oh=42b4a2617160ad6e161c870b0da0c15a&oe=5950DB99")
